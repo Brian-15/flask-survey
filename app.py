d210addd8fccf40d6b5f9388a21589e093c26429
Brian-15 <bcirieco@gmail.com>
2021-08-11 12:13:17 -0700

    user cannot answer questions out of order by manually typing the URL, and will be redirected to the current question

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import surveys
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret123"
 
 responses = []
 
@@ -10,11 +11,16 @@ def root():
 
 @app.route("/question/<int:num>", methods = ["GET"])
 def question(num):
+
+    if num != len(responses):
+        flash("Error: Invalid Question")
+        return redirect(f"/question/{len(responses)}")
+
     if num == len(surveys.satisfaction_survey.questions) - 1:
-        return redirect("/thankyou")
+        return redirect("/thankyou")    
     
     q = surveys.satisfaction_survey.questions[num]
-    
+
     return render_template("question.html",
         q=q,
         num=num
